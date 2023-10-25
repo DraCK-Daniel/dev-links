68e7e55ff08152c3b168b5ba338442d12489b990
Daniel Rodrigues <kayque_daniel@hotmail.com>
2023-10-25 19:27:29 -0300

    style: modified background image to NextImage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 
 import { inter } from '@/styles/fonts'
 import type { Metadata } from 'next'
+import Image from 'next/image'
 
 export const metadata: Metadata = {
   title: 'Daniel Rodrigues | Dev Links',
@@ -18,7 +19,16 @@ export default function RootLayout({
       <body
         className={`${inter.variable} bg-background text-foreground dark relative flex min-h-screen items-center justify-center font-sans`}
       >
-        <div className="bg-home absolute inset-0 -z-10 bg-cover bg-center bg-no-repeat" />
+        <div className="absolute inset-0 -z-10 h-auto w-auto">
+          <Image
+            src="/glow-homepage.webp"
+            alt="Background gradient Home Page"
+            fill
+            sizes="100%"
+            className="object-cover"
+          />
+        </div>
+
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="w-full max-w-lg space-y-8 px-6">
       <section className="grid place-items-center space-y-4">
-        <div className="grid h-28 w-28 place-items-center rounded-full border-2">
+        <div className="rounded-full border-2 bg-gradient-to-br from-sky-500 to-violet-700 p-1">
           <Avatar className="h-24 w-24">
             <AvatarImage src="https://avatars.githubusercontent.com/u/97995126?v=4" />
             <AvatarFallback>DR</AvatarFallback>
@@ -27,7 +27,7 @@ export default function Home() {
             href={link.href}
             target="_blank"
             rel="noreferrer"
-            className="inline-flex h-12 w-full items-center justify-center whitespace-nowrap rounded-md bg-white/5 px-4 text-sm font-medium backdrop-blur-sm transition-colors duration-300 hover:bg-white/10"
+            className="inline-flex h-12 w-full items-center justify-center whitespace-nowrap rounded-md bg-white/5 px-4 text-sm font-medium backdrop-blur-sm transition-all duration-300 will-change-transform hover:scale-105 hover:bg-white/10"
           >
             <link.icon className="mr-2 h-5 w-5" />
             {link.name}
@@ -42,7 +42,7 @@ export default function Home() {
             href={social.href}
             target="_blank"
             rel="noreferrer"
-            className="text-muted hover:text-foreground transition-all duration-300 hover:scale-125"
+            className="text-foreground/50 hover:text-foreground transition-all duration-300 hover:scale-125"
           >
             <social.icon className="h-6 w-6" />
           </a>
