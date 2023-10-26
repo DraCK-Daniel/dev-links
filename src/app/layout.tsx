3abdf27266f0afaf6d4c7f064c08b5ae31da6634
Daniel Rodrigues <kayque_daniel@hotmail.com>
2023-10-25 21:20:55 -0300

    chore: add open-graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   openGraph: {
     images: [
       {
-        url: 'https://dev-links-tawny.vercel.app/og-image.png',
+        url: '/og-image.png',
         width: 1200,
         height: 630,
       },
