849afca7af5562221e736feb968e31a1b213d293
Daniel Rodrigues <kayque_daniel@hotmail.com>
2023-10-25 21:01:04 -0300

    fix: fix href url

--- src/app/(home)/components/section-buttons.tsx
+++ src/app/(home)/components/section-buttons.tsx
@@ -6,7 +6,7 @@ export const SectionButtons = () => {
       {LINKS.map((link) => (
         <a
           key={link.name}
-          href={link.name}
+          href={link.href}
           target="_blank"
           rel="noreferrer"
           className="inline-flex h-12 w-full items-center justify-center whitespace-nowrap rounded-md bg-white/5 px-4 text-sm font-medium backdrop-blur-sm transition-all duration-300 will-change-transform hover:scale-105 hover:bg-white/10"
